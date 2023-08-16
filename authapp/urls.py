f71b75f1c8e33e5610117d5d55261ce1b6929506
Ajit <mauryaajit.am@outlook.com>
2023-08-16 15:29:14 +0530

    add view logic for login

--- authapp/urls.py
+++ authapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home',views.test),
+    path('home',views.login),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import UserAccount,UserLogInData
+from .models import EmailValidationStatus
+from django.shortcuts import get_list_or_404
 
 # Create your views here.
-def test(request):
-    return render(request, 'login.html')
+def login(request):
+    try:
+        registeredUser = get_list_or_404(UserLogInData, user_id = request.user.id)
+        emailStatus = EmailValidationStatus.objects.get(pk = registeredUser.email_validation_status)
+        if emailStatus.email_validation_status == False:
+            context = {
+                'message' : 'Email is not varified. An email was sent to your email id with verification link, link will be only active for 10 minutes',
+            }
+            return render(request, 'login_failed.html', context)
+        context = {
+            'username' : registeredUser.login_name,
+        }
+        return render(request, 'logged.html', context)
+    except Exception as e:
+        return render(request, 'Login_failed.html')
