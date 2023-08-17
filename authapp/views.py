f1c1d43269e59995b80b22720ee23e6a1d62b7cf
Ajit <mauryaajit.am@outlook.com>
2023-08-17 15:23:45 +0530

    corrected login endpoint

--- authapp/views.py
+++ authapp/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from .models import UserAccount,UserLogInData
 from .models import EmailValidationStatus
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def login(request):
+    if request.method != 'POST':
+        return render(request,'login.html',{'messege' : 'Only post method are allowed!'})
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    hashed_password = make_password(password)
+    if email is None and password is None:
+        return render(request,'login.html',{'messege' : 'Please provide email and password'})
     try:
-        registeredUser = get_list_or_404(UserLogInData, user_id = request.user.id)
+        registeredUser = get_object_or_404(UserLogInData,email_address = email, password_hash = hashed_password)
         emailStatus = EmailValidationStatus.objects.get(pk = registeredUser.email_validation_status)
         if emailStatus.email_validation_status == False:
             context = {
                 'message' : 'Email is not varified. An email was sent to your email id with verification link, link will be only active for 10 minutes',
             }
-            return render(request, 'login_failed.html', context)
+            return render(request, 'verifications.html', context)
         context = {
             'username' : registeredUser.login_name,
         }
