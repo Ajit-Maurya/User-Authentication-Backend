d7becb770cadb6af9a3d0fabde852a88c999cc92
Ajit <mauryaajit.am@outlook.com>
2023-08-18 15:07:17 +0530

    updated login endpoint to handle errors

--- AuthProject/urls.py
+++ AuthProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',include('authapp.urls')),
+    path('',include('authapp.urls')),
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import UserAccount,UserLogInData
-from .models import EmailValidationStatus
-from django.shortcuts import get_object_or_404
-from django.contrib.auth.hashers import make_password
+from .models import EmailValidationStatus,UserLoginDataExternal
+# from django.shortcuts import get_object_or_404
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 def login(request):
@@ -10,24 +10,27 @@ def login(request):
         return render(request,'login.html',{'messege' : 'Only post method are allowed!'})
     email = request.POST.get('email')
     password = request.POST.get('password')
-    hashed_password = make_password(password)
-    if email is None and password is None:
+    
+    if email is None or password is None:
         return render(request,'login.html',{'messege' : 'Please provide email and password'})
+    hashed_password = make_password(password) 
     try:
-        registeredUser = get_object_or_404(UserLogInData,email_address = email, password_hash = hashed_password)
-        emailStatus = EmailValidationStatus.objects.get(pk = registeredUser.email_validation_status)
-        if emailStatus.email_validation_status == False:
-            context = {
-                'message' : 'Email is not varified. An email was sent to your email id with verification link, link will be only active for 10 minutes',
-            }
-            return render(request, 'verifications.html', context)
-        context = {
-            'username' : registeredUser.login_name,
-        }
-        return render(request, 'logged.html', context)
-    except Exception as e:
-        context = {
-            'message' : 'user does not exists',
-        }
-        return render(request, 'login.html', context)
+        user = UserLogInData.objects.get(email_address=email)
+        if not check_password(hashed_password,user.password_hash):
+            raise UserLogInData.DoesNotExist
 
+        email_status = EmailValidationStatus.objects.get(pk=user.email_validation_status)
+        if not email_status.email_validation_status:
+            context = {'message':'email is not verified'}
+            return render(request, 'verification.html',context)
+        context = {'token' : user.confirmation_token}
+        return render(request,'logged.html',context)
+    except UserAccount.DoesNotExist:
+        context = {'message' : 'user does not exists',}
+        return render(request, 'login.html', context)
+    except EmailValidationStatus.DoesNotExist:
+        context = {'message' : 'email is not registered'}
+        return render(request, 'login.html',context)
+
+def home(request):
+    return render(request, 'login.html')
