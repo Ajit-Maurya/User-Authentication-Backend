45e4862e815c3f2a1133b584f607ae974056bf4b
Ajit <mauryaajit.am@outlook.com>
2023-08-29 12:20:53 +0530

    removed everything

--- authapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views,signup
-
-urlpatterns = [
-    path('',views.home),
-    path('login',views.login,name='login'),
-    path('signup',signup.signup,name='signup'),
-    # path('externalProvider',views.externalProvider,name='externalProvider')
-]
