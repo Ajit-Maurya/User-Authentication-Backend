81aed2de92abdc8d1b4d7af700c3c68c74b079d6
Ajit <mauryaajit.am@outlook.com>
2023-08-29 15:10:21 +0530

    created verify logic to verify account

--- authap/urls.py
+++ authap/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . import views,signup
+from . import views,signup,verify
 
 urlpatterns = [
     path('',views.home),
     path('login',views.login,name='login'),
     path('signup',signup.signup,name='signup'),
     # path('externalProvider',views.externalProvider,name='externalProvider')
+    path('verify',verify.verify,name='verify'),
 ]
--- authap/verify.py
+++ authap/verify.py
@@ -3,11 +3,14 @@ from .models import UserLogInData
 
 
 def verify(request):
-    confirmation_token = request.GET.get('token')
-    if UserLogInData.objects.filter(confirmation_token=confirmation_token).exists():
-        user = UserLogInData.objects.get(confirmation_token=confirmation_token)
+    token = request.GET.get('token')
+    # if UserLogInData.objects.filter(confirmation_token=token).exists():
+    try:
+        user = UserLogInData.objects.get(confirmation_token=token)
         user.email_validation_status = True
         user.save()
         return render(request,'error.html',{'message' : 'Your email is now verified, Login again'})
-    else:
-        return render(request,'error.html',{'message': 'Unauthorized'})
+    except UserLogInData.DoesNotExist:
+        return render(request,'error.html',{'message':'invalid request'})
+    # else:
+    #     return render(request,'error.html',{'message': 'Unauthorized'})
